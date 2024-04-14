df1e94fe99be375ee560a0cf3006c5c4c3407fad
liquidloose <ronaldlussier83@gmail.com>
2024-04-14 08:12:18 +0000

    Cleaning up

--- patterns/web-dev-hero.php
+++ patterns/web-dev-hero.php
@@ -1,48 +1,29 @@
 <?php
-
 /**
  * Title: web-dev-hero
  * Slug: rlussier-portfolio/web-dev-hero
- * Categories: hero
- * Inserter: yes
+ * Categories: hidden
+ * Inserter: no
  */
 ?>
+<!-- wp:kadence/rowlayout {"uniqueID":"200_b2f383-fb","collapseGutter":"none","customRowGutter":[0,"",""],"columnGutter":"none","customGutter":[0,"",""],"colLayout":"equal","bgColor":"#636363","bgColorClass":"contrast-2","padding":["","","sm",""],"kbVersion":2} -->
+<!-- wp:kadence/column {"background":"#d8613c","borderWidth":["","","",""],"uniqueID":"200_52cc38-cf","textAlign":["center","",""],"direction":["horizontal","",""],"gutter":[16,"",""],"gutterVariable":["sm","",""],"rowGap":[16,"",""],"rowGapVariable":["sm","",""],"flexBasis":[458,"",""],"verticalAlignment":"middle","kbVersion":2,"kadenceBlockCSS":"selector {\nheight: 100% !important;\nbackground: #D8613C;\n}"} -->
+<div class="wp-block-kadence-column kadence-column200_52cc38-cf kb-section-dir-horizontal"><div class="kt-inside-inner-col"><!-- wp:kadence/rowlayout {"uniqueID":"200_52e24c-0f","columns":1,"colLayout":"equal","align":"center","verticalAlignment":"middle","topSepHeight":0,"bottomSepHeight":0,"topSepWidth":100,"backgroundSettingTab":"gradient","inheritMaxWidth":true,"gradient":"linear-gradient(135deg,rgb(216,97,60) 4%,rgb(194,169,144) 54%,rgb(194,169,144) 99%)","padding":["","","",""],"kbVersion":2,"kadenceBlockCSS":"selector {\nheight: 100%  ;\n}"} -->
+<!-- wp:kadence/column {"borderWidth":["","","",""],"uniqueID":"200_39dda8-76","direction":["horizontal","",""],"justifyContent":["center","",""],"rowGap":[0,"",""],"rowGapVariable":["none","",""],"flexBasis":[535,"",""],"verticalAlignment":"middle","kbVersion":2} -->
+<div class="wp-block-kadence-column kadence-column200_39dda8-76 kb-section-dir-horizontal"><div class="kt-inside-inner-col"><!-- wp:paragraph {"fontSize":"xx-large"} -->
+<p class="has-xx-large-font-size"><?php echo __('Web Developer', 'rlussier-portfolio');?></p>
+<!-- /wp:paragraph -->
 
-<!-- wp:kadence/rowlayout {"uniqueID":"960_b3949a-12","collapseGutter":"none","customRowGutter":[0,"",""],"columnGutter":"none","customGutter":[0,"",""],"colLayout":"equal","bgColor":"#636363","verticalAlignment":"middle","maxWidthUnit":"vw","inheritMaxWidth":true,"bgColorClass":"contrast-2","padding":["","","sm",""],"kbVersion":2} -->
-<!-- wp:kadence/column {"background":"#d8613c","borderWidth":["","","",""],"uniqueID":"960_6adb84-0b","textAlign":["center","",""],"direction":["horizontal","",""],"gutter":[0,"",""],"gutterVariable":["none","",""],"rowGap":[0,"",""],"rowGapVariable":["none","",""],"flexBasis":[542,"",""],"verticalAlignment":"middle","kbVersion":2,"kadenceBlockCSS":"selector {\nheight: 100% !important;\nbackground: #D8613C;\n}"} -->
-<div class="wp-block-kadence-column kadence-column960_6adb84-0b kb-section-dir-horizontal">
-    <div class="kt-inside-inner-col"><!-- wp:kadence/rowlayout {"uniqueID":"960_c1bb66-c6","columns":1,"colLayout":"equal","align":"center","verticalAlignment":"middle","topSepHeight":0,"bottomSepHeight":0,"maxWidthUnit":"vw","topSepWidth":100,"backgroundSettingTab":"gradient","responsiveMaxWidth":["",97],"gradient":"linear-gradient(135deg,rgb(216,97,60) 4%,rgb(194,169,144) 54%,rgb(194,169,144) 99%)","padding":["","","",""],"kbVersion":2,"kadenceBlockCSS":"selector {\nheight: 100%  ;\n}"} -->
-        <!-- wp:kadence/column {"borderWidth":["","","",""],"uniqueID":"960_52b6eb-56","direction":["horizontal","",""],"justifyContent":["center","",""],"wrapContent":["wrap","",""],"rowGap":[0,"",""],"rowGapVariable":["none","",""],"flexBasis":[535,"",""],"verticalAlignment":"middle","padding":["xxs","","xs",""],"kbVersion":2} -->
-        <div class="wp-block-kadence-column kadence-column960_52b6eb-56 kb-section-dir-horizontal">
-            <div class="kt-inside-inner-col"><!-- wp:paragraph {"fontSize":"xx-large"} -->
-                <p class="has-xx-large-font-size">Web Developer</p>
-                <!-- /wp:paragraph -->
-
-                <!-- wp:paragraph -->
-                <p>I like to build things</p>
-                <!-- /wp:paragraph -->
-            </div>
-        </div>
-        <!-- /wp:kadence/column -->
-        <!-- /wp:kadence/rowlayout -->
-    </div>
-</div>
+<!-- wp:paragraph -->
+<p><?php echo __('I like to build things', 'rlussier-portfolio');?></p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:kadence/column -->
+<!-- /wp:kadence/rowlayout --></div></div>
 <!-- /wp:kadence/column -->
 
-<!-- wp:kadence/column {"borderWidth":["","","",""],"uniqueID":"960_04560f-28","direction":["horizontal","",""],"justifyContent":["center","",""],"rowGap":[0,"",""],"rowGapVariable":["none","",""],"flexBasis":[535,"",""],"verticalAlignment":"middle","kbVersion":2} -->
-<div class="wp-block-kadence-column kadence-column960_04560f-28 kb-section-dir-horizontal">
-    <div class="kt-inside-inner-col"><!-- wp:kadence/image {"id":884,"sizeSlug":"large","useRatio":true,"linkDestination":"none","uniqueID":"960_3ea381-97"} -->
-        <figure class="wp-block-kadence-image kb-image960_3ea381-97 size-large kb-image-is-ratio-size">
-            <div class="kb-is-ratio-image kb-image-ratio-land43"><img src="http://192.168.1.22:8001/wp-content/uploads/2024/02/bw_ron-1024x768.jpg" alt="" class="kb-img wp-image-884" /></div>
-        </figure>
-        <!-- /wp:kadence/image -->
-    </div>
-</div>
-<!-- /wp:kadence/column -->
-
-<!-- wp:kadence/column {"id":2,"borderWidth":["","","",""],"uniqueID":"960_409db7-e2","direction":["horizontal","",""],"gutter":[0,"",""],"gutterVariable":["none","",""],"rowGap":[0,"",""],"rowGapVariable":["none","",""],"verticalAlignment":"","kbVersion":2} -->
-<div class="wp-block-kadence-column kadence-column960_409db7-e2 kb-section-dir-horizontal">
-    <div class="kt-inside-inner-col"></div>
-</div>
+<!-- wp:kadence/column {"id":2,"borderWidth":["","","",""],"uniqueID":"200_408ab2-51","direction":["horizontal","",""],"gutter":[0,"",""],"gutterVariable":["none","",""],"rowGap":[0,"",""],"rowGapVariable":["none","",""],"verticalAlignment":"middle","kbVersion":2} -->
+<div class="wp-block-kadence-column kadence-column200_408ab2-51 kb-section-dir-horizontal"><div class="kt-inside-inner-col"><!-- wp:image {"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/bw_ron-1024x768.jpg" alt="" class=""/></figure>
+<!-- /wp:image --></div></div>
 <!-- /wp:kadence/column -->
 <!-- /wp:kadence/rowlayout -->
